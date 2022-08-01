774d093582520fde6cdf082d0e75d8999fe9189f
Ismail Hossen <ismailhossendev@gmail.com>
2022-08-01 16:58:32 +0600

    frist problem solved

---
+++ frist-problem.js
@@ -0,0 +1,7 @@
+var fruits = ['apple', 'banana', 'Orange'];
+console.log(fruits.indexOf('banana'));
+fruits[1] = 'Mango';
+console.log(fruits)
+fruits.pop()
+fruits.push("Watermelon")
+console.log(fruits)
